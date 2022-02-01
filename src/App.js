2bc67783ef7db598b0e3fdc2f588f5116af22df9
cliffmax85 <cliffmax85@gmail.com>
2022-01-31 17:34:58 -0800

    worjking on diplsay slogans

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   // you'll need to track a city name, which starts as the city name of your choice.
   const [cityName, setCityName] = useState('Boring');
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
-  const [slogans, setSlogans] = useState(['It is sunny', 'It might also be rainy']);
+  const [slogan, setSlogan] = useState(['It is sunny', 'It might also be rainy']);
 
   return (
     <div className="App">
@@ -44,9 +44,9 @@ function App() {
           <CastleDropdown setCastleId={setCastleId} />
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
-        <SloganForm slogans={slogans} setSlogans={setSlogans} />
+        <SloganForm setSlogan={setSlogan} slogan={slogan} />
         {/* here, the SloganList component takes the array of slogans that lives in state */}
-        <SloganList slogans={slogans} />
+        <SloganList slogan={slogan} />
       </div>
     </div>
   );
--- src/SloganList.js
+++ src/SloganList.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import Slogan from './Slogan';
 
-export default function SloganList(props) {
+export default function SloganList({ slogan }) {
   return (
     <section>
       <h2>Your slogans:</h2>
       {/* This kind of component should be familiar by now. It takes in (as props) an array of slogans */}
       {/* Then map over those slogans and for each slogan, render a Slogan component. Supply a key and a slogan as props to each Slogan component */}
-      <ul className='slogans-list'>
-        {
-          props.slogans.map((slogan, i) => <Slogan key={`${slogan}-${i}`} slogan={slogan} />
-          )
-        }
-      </ul>
+      {
+        slogan.map((slogans, i) => 
+          <Slogan key={`${slogans}-${i}`} slogans={slogans} />
+        )
+      }
     </section>
   );
 }
